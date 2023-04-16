66ad8f35b95e91f938f635d6f05164e4c17038e0
Dombara <yash13506@gmail.com>
2023-04-16 23:50:52 +0000

    first year form almost done

--- UI_Yash/main.py
+++ UI_Yash/main.py
@@ -12,15 +12,17 @@ app.title("App")
 #--------Functions--------
 def year1():
     s_year_frame.grid_forget()
-    f_year_frame.grid_propagate(0)
-    f_year_frame.grid(row=0,column=1)    
+    f_year_frame.grid_propagate()
+    # f_year_frame.grid(row=0,column=1) 
+    f_year_frame.grid(row=0,column=1,sticky="N",padx=5,pady=5)   
 
 
 
 def year2():
     f_year_frame.grid_forget()
     s_year_frame.grid_propagate(0)
-    s_year_frame.grid(row=0,column=1)
+    # s_year_frame.grid(row=0,column=1)
+    s_year_frame.grid(row=0,column=1,sticky="N",padx=5,pady=5)   
 
 
 
@@ -29,7 +31,7 @@ def year2():
 
 
 frame1=CTkFrame(app,width=185,height=height)
-frame1.grid(row=0,column=0,padx=5,pady=5)
+frame1.grid(row=0,column=0,padx=5,pady=5,sticky="E")
 frame1.grid_propagate(0)
 
 year_var=IntVar()
@@ -46,8 +48,8 @@ s_year_buton.grid(padx=10,pady=10)
 
 
 #--------First Year Frame--------
-f_year_frame=CTkScrollableFrame(app,width=1150,height=height-80)
-f_year_frame.grid(row=0,column=1,sticky="N")
+f_year_frame=CTkScrollableFrame(app,width=1150,height=height-80,orientation="vertical")
+f_year_frame.grid(row=0,column=1,sticky="N",padx=5,pady=5)
 f_year_frame.grid_propagate()
 # f_label=CTkLabel(f_year_frame,text='1st year')
 # f_label.grid()
@@ -55,7 +57,7 @@ f_year_frame.grid_propagate()
 
 #--------Frame content--------
 reg_no_label = CTkLabel(f_year_frame, text="Registration Number")
-reg_no_label.grid(padx=500,pady=5,sticky="E")
+reg_no_label.grid(padx=500,pady=5)
 reg_no_entry = CTkEntry(f_year_frame)
 reg_no_entry.grid(padx=500,pady=5)
 
@@ -83,7 +85,7 @@ other_radiobtn.grid(padx=500,pady=10)
 mo_var = BooleanVar()
 py_var = BooleanVar()
 engg_var = BooleanVar()
-program = CTkLabel(f_year_frame, text="Program")
+program = CTkLabel(f_year_frame, text="Program:")
 program.grid(padx=500,pady=10)
 mo_btn = CTkCheckBox(f_year_frame, text="MO", variable=mo_var)
 mo_btn.grid(padx=500,pady=10)
@@ -94,12 +96,18 @@ engg_btn.grid(padx=500,pady=10)
 
 
 #--------GEN/CSP--------
+
+
+
+
 gen_csp_var= StringVar()
-gen_csp_label = CTkLabel(f_year_frame, text="GEN/CSP:")
+gen_csp_label = CTkLabel(f_year_frame, text="GEN/CSP:" )
 gen_csp_label.grid(padx=500,pady=10)
 
 gen_radiobtn = CTkRadioButton(f_year_frame, text="GEN",variable=gen_csp_var, value="GEN")
 gen_radiobtn.grid(padx=500,pady=10)
+
+
 
 csp_radiobtn = CTkRadioButton(f_year_frame,text="CSP", variable=gen_csp_var, value="CSP")
 csp_radiobtn.grid(padx=500,pady=10)
@@ -108,8 +116,8 @@ csp_radiobtn.grid(padx=500,pady=10)
 
 #--------SC/ST/OBC--------
 sc_st_obc_var= StringVar()
-gen_csp_label = CTkLabel(f_year_frame, text="GEN/CSP:")
-gen_csp_label.grid(padx=500,pady=10)
+sc_st_obc_label = CTkLabel(f_year_frame, text="SC/ST/OBC:")
+sc_st_obc_label.grid(padx=500,pady=10)
 
 sc_radiobtn = CTkRadioButton(f_year_frame, text="SC",variable=sc_st_obc_var, value="SC")
 sc_radiobtn.grid(padx=500,pady=10)
@@ -119,6 +127,84 @@ st_radiobtn.grid(padx=500,pady=10)
 
 obc_radiobtn = CTkRadioButton(f_year_frame,text="OBC", variable=sc_st_obc_var, value="OBC")
 obc_radiobtn.grid(padx=500,pady=10)
+
+
+#--------PwD/FF/ESM--------
+
+pwd_ff_esm_var=StringVar()
+pwd_ff_esm_label=CTkLabel(f_year_frame, text="PwD/FF/ESM")
+pwd_ff_esm_label.grid(padx=500,pady=10)
+
+pwd_radiobtn = CTkRadioButton(f_year_frame, text="PwD",variable=sc_st_obc_var, value="PwD")
+pwd_radiobtn.grid(padx=500,pady=10)
+
+ff_radiobtn = CTkRadioButton(f_year_frame,text="FF", variable=sc_st_obc_var, value="FF")
+ff_radiobtn.grid(padx=500,pady=10)
+
+esm_radiobtn = CTkRadioButton(f_year_frame,text="ESM", variable=sc_st_obc_var, value="ESM")
+esm_radiobtn.grid(padx=500,pady=10)
+
+
+
+
+#--------GN/NRI/LA/OGA--------
+
+gn_nri_la_oga_var=StringVar()
+
+gn_nri_la_oga_label=CTkLabel(f_year_frame, text="GN/NRI/LA/OGA")
+gn_nri_la_oga_label.grid(padx=500,pady=10)
+
+gn_radiobtn = CTkRadioButton(f_year_frame, text="GN",variable=gn_nri_la_oga_var, value="GN")
+gn_radiobtn.grid(padx=500,pady=10)
+
+nri_radiobtn = CTkRadioButton(f_year_frame,text="NRI", variable=gn_nri_la_oga_var, value="NRI")
+nri_radiobtn.grid(padx=500,pady=10)
+
+la_radiobtn = CTkRadioButton(f_year_frame,text="LA", variable=gn_nri_la_oga_var, value="LA")
+la_radiobtn.grid(padx=500,pady=10)
+
+
+oga_radiobtn = CTkRadioButton(f_year_frame,text="OGA", variable=gn_nri_la_oga_var, value="OGA")
+oga_radiobtn.grid(padx=500,pady=10)
+
+
+
+
+
+ssc_eng_label=CTkLabel(f_year_frame, text="SSC English marks:")
+ssc_eng_label.grid(padx=500,pady=5)
+ssc_eng_entry = CTkEntry(f_year_frame)
+ssc_eng_entry.grid(padx=500,pady=5)
+
+
+
+
+ssc_mat_label=CTkLabel(f_year_frame, text="SSC Maths marks:")
+ssc_mat_label.grid(padx=500,pady=5)
+ssc_mat_entry = CTkEntry(f_year_frame)
+ssc_mat_entry.grid(padx=500,pady=5)
+
+
+
+
+ssc_sci_label=CTkLabel(f_year_frame, text="SSC Science marks:")
+ssc_sci_label.grid(padx=500,pady=5)
+ssc_sci_entry = CTkEntry(f_year_frame)
+ssc_sci_entry.grid(padx=500,pady=5)
+
+
+
+
+ssc_total_label=CTkLabel(f_year_frame, text="SSC Total marks:")
+ssc_total_label.grid(padx=500,pady=5)
+ssc_total_entry = CTkEntry(f_year_frame)
+ssc_total_entry.grid(padx=500,pady=5)
+
+
+
+
+
+
 
 
 
